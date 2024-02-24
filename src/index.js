e3c76fa2df8b8e2313a800cbcefc1a3217a07c64
Thomas Steifler <steiflerthomas@gmail.com>
2024-02-24 17:19:53 +0100

    add card styling

--- src/index.js
+++ src/index.js
@@ -23,16 +23,24 @@ function removeBookfromLibrary(book) {
 }
 
 const test = new Book("A Game of Thrones", "George R. R. Martin", 694, false)
-const test2 = new Book("The Last Wish", "Andrzej Sapkowski", 288, false)
+const test2 = new Book("The Last Wish", "Andrzej Sapkowski", 288, true)
+const test3 = new Book("The Last Wish", "Andrzej Sapkowski", 288, false)
+const test4 = new Book("The Last Wish", "Andrzej Sapkowski", 288, true)
+const test5 = new Book("The Last Wish", "Andrzej Sapkowski", 288, true)
+const test6 = new Book("The Last Wish", "Andrzej Sapkowski", 288, false)
 
 addBookToLibrary(test)
 addBookToLibrary(test2)
+addBookToLibrary(test3)
+addBookToLibrary(test4)
+addBookToLibrary(test5)
+addBookToLibrary(test6)
 
 function createCard(book) {
     const card = document.createElement("div");
     card.classList.add("card")
 
-    const title = document.createElement("p")
+    const title = document.createElement("h2")
     title.innerText = book.title;
     card.appendChild(title)
 
@@ -41,15 +49,33 @@ function createCard(book) {
     card.appendChild(author)
 
     const pages = document.createElement("p")
-    pages.innerText = book.pages
+    pages.innerText = `${book.pages} pages`
     card.appendChild(pages)
+
+    const status = document.createElement("p")
+    if(book.readStatus) {
+        status.innerText = "Finished reading"
+    }
+    else {
+        status.innerText = "Not read yet"
+    }
+    card.appendChild(status)
+
+    const buttons = document.createElement("div")
+    buttons.classList.add("buttons")
 
     const removeButton = document.createElement("button")
     removeButton.innerText = "Remove"
     removeButton.addEventListener("click", () => {
         removeBookfromLibrary(book)
     })
-    card.appendChild(removeButton)
+    buttons.appendChild(removeButton)
+
+    const statusButton = document.createElement("button")
+    statusButton.innerText = "Toggle read status"
+    buttons.appendChild(statusButton)
+
+    card.appendChild(buttons)
 
     main.appendChild(card)
 }
